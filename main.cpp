4d7d8d841c27cc1c5b29c8e9cb4c783005ba754f
Sergey Chupligin <neochapay@gmail.com>
2015-12-21 19:23:20 +0300

    Connect model` to main.cpp

--- main.cpp
+++ main.cpp
@@ -2,8 +2,9 @@
 #include <QQmlApplicationEngine>
 #include <QQmlContext>
 #include <QScreen>
+#include <QtQml>
 
-#include "src/dbadapter.h"
+#include "src/models/tasksqlmodel.h"
 #include "src/models/taskmodel.h"
 
 int main(int argc, char *argv[])
@@ -12,14 +13,17 @@ int main(int argc, char *argv[])
     app.setApplicationName("simpletask");
     app.setOrganizationName("simpletasks");
 
-    dbAdapter *db = new dbAdapter();
+    TaskSqlModel *taskSqlModel = new TaskSqlModel();
 
     QScreen *screen = qApp->primaryScreen();
     int dpi = screen->physicalDotsPerInch() * screen->devicePixelRatio();
     qreal dp = dpi / 160.f;
 
+    qmlRegisterType<Task>("taskAdapter",1,0,"Task");
+
     QQmlApplicationEngine engine;
     engine.rootContext()->setContextProperty("dp", dp);
+    engine.rootContext()->setContextProperty("TaskSqlModel", taskSqlModel);
     engine.rootContext()->setContextProperty("isMobile", true);
     engine.load(QUrl(QStringLiteral("qrc:/main.qml")));
 
